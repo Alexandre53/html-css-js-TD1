14ae7df41b6f6c1df422c1d249e00e00123dc6b6
Alexandre53 <59557064+Alexandre53@users.noreply.github.com>
2020-02-03 09:50:51 +0100

    Add files via upload

---
+++ question 2/td1.js
@@ -0,0 +1,19 @@
+/*
+	Manipulation d'une adresse IP
+
+*/
+
+var part1=parti1;
+var part2=parti2;
+var part3=parti3;
+var part4=parti4;
+
+
+var orginalIP = part1+"."+part2+"."+part3+"."+part4;
+
+part4=99;
+
+var newlIP = part1+"."+part2+"."+part3+"."+part4;
+
+document.write(orginalIP);
+
