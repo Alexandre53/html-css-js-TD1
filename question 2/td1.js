d186215179ecaa42668ad68df502ab0669e96a69
Alexandre53 <59557064+Alexandre53@users.noreply.github.com>
2020-02-10 09:31:15 +0100

    Update td1.js

--- question 2/td1.js
+++ question 2/td1.js
@@ -1,17 +1,14 @@
 /*
 	Manipulation d'une adresse IP
-
+    
 */
 
-document.form.addEventListener("submit", myfunction);
 
-function myfunction()
-{
-	var Ip1=document.form.elements.parti1.value;
-    var Ip2=document.form.elements.parti2.value;
-    var Ip3=document.form.elements.parti3.value;
-    var Ip4=document.form.elements.parti4.value;
 
-console.log(Ip1+"."+Ip2+"."+ip3+"."+ip4);
+
+function valenter() {
+  var adress = document.getElementById("adresse").value;
+  document.getElementById("adr").innerHTML =(adress);
 
 }
+                  
